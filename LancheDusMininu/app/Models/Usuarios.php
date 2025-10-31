dfd60a6da44ba565df7c41d4cb9bc071af2d7371
ThiAssumpcao09 <thiago.assump1210@gmail.com>
2025-10-30 21:18:06 -0300

    tentando criar usuarios

---
+++ LancheDusMininu/app/Models/Usuarios.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+
+class Usuarios
+{
+    public static function all()
+    {
+        return DB::table('usuarios')->get();
+    }
+
+    public static function find($id)
+    {
+        return DB::table('usuarios')->where('id', $id)->first();
+    }
+
+    public static function create($data)
+    {
+        return DB::table('usuarios')->insert($data);
+    }
+
+    public static function updateUser($id, $data)
+    {
+        return DB::table('usuarios')->where('id', $id)->update($data);
+    }
+
+    public static function deleteUser($id)
+    {
+        return DB::table('usuarios')->where('id', $id)->delete();
+    }
+}
