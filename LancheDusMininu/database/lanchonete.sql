447df0b0865011e1bbe7ff8e5a90c6a262e28963
ThiAssumpcao09 <thiago.assump1210@gmail.com>
2025-11-01 14:03:07 -0300

    Update lanchonete.sql

--- LancheDusMininu/database/lanchonete.sql
+++ LancheDusMininu/database/lanchonete.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25/10/2025 às 20:38
+-- Tempo de geração: 01/11/2025 às 18:01
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -162,6 +162,35 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `lanches`
+--
+
+CREATE TABLE `lanches` (
+  `id` int(11) NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `lanches`
+--
+
+INSERT INTO `lanches` (`id`, `imagem`, `titulo`, `descricao`, `preco`) VALUES
+(1, 'assets/img/img-1.png', 'X VGtariano', 'Ovo, Mussarela, Palmito, Tomate Seco, Alface, Rúcula e Tomate no Pão de Hambúrguer', 29.40),
+(2, 'assets/img/img-1.png', 'Batata Individual com Molho Cheddar e Farofa de Bacon', '(150Gr Crua) Batata com Molho Cheddar e Farofa de Bacon', 17.50),
+(3, 'assets/img/img-1.png', 'Mozza Burger', 'Hambúrguer 200gr, Queijo Mussarela Empanada, Bacon Molho Especial, Pão Brioche', 29.70),
+(4, 'assets/img/img-1.png', 'Cheddar Burger', 'Hambúrguer 180gr, Queijo Cheddar, Cebola Caramelizada e Pão Australiano', 27.90),
+(5, 'assets/img/img-1.png', 'Combo Familiar', '4 Hambúrgueres + 2 Batatas Grandes + 2 Refrigerantes 2L', 99.90),
+(6, 'assets/img/img-1.png', 'Milkshake', 'Milkshake cremoso nos sabores Chocolate, Morango e Ovomaltine', 14.90),
+(7, 'assets/img/img-1.png', 'Onion Rings', 'Cebolas empanadas crocantes acompanhadas de molho especial', 16.90),
+(8, 'assets/img/img-1.png', 'Smash Burger', 'Duas carnes 100gr prensadas na chapa, queijo e molho da casa', 24.90),
+(9, 'assets/img/img-1.png', 'Hot Dog Especial', 'Salsicha dupla, purê, batata palha, queijo e molho especial da casa', 15.90);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `migrations`
 --
 
@@ -241,33 +270,6 @@ CREATE TABLE `sessions` (
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
 ('RuCNKOOJcHZHPc595Z3QGXaTTym4z2hOlDXh5Lky', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiUVdkUHFRY1k4clNNaU50VFdMV1hXVkY3b2ZwVnE0SFJrNWN3NFdCMCI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9jYWRhc3RybyI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1761416950);
 
-----------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `cod_usuario` int(11) NOT NULL,
-  `nome_usuario` varchar(50) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `senha` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`cod_usuario`);
-
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT;
-
-
 -- --------------------------------------------------------
 
 --
@@ -284,6 +286,19 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `cod_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(50) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
@@ -354,6 +369,12 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `lanches`
+--
+ALTER TABLE `lanches`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `migrations`
 --
 ALTER TABLE `migrations`
@@ -394,6 +415,12 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`cod_usuario`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
@@ -428,52 +455,22 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `lanches`
+--
+ALTER TABLE `lanches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de tabela `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de tabela `pedidos`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
-ALTER TABLE `pedidos`
-  MODIFY `cod_pedido` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `pratos`
---
-ALTER TABLE `pratos`
-  MODIFY `cod_prato` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `composicao`
---
-ALTER TABLE `composicao`
-  ADD CONSTRAINT `composicao_ibfk_1` FOREIGN KEY (`cod_prato`) REFERENCES `pratos` (`cod_prato`),
-  ADD CONSTRAINT `composicao_ibfk_2` FOREIGN KEY (`cod_ingrediente`) REFERENCES `ingredientes` (`cod_ingrediente`);
-
---
--- Restrições para tabelas `itens_pedidos`
---
-ALTER TABLE `itens_pedidos`
-  ADD CONSTRAINT `itens_pedidos_ibfk_1` FOREIGN KEY (`cod_pedido`) REFERENCES `pedidos` (`cod_pedido`),
-  ADD CONSTRAINT `itens_pedidos_ibfk_2` FOREIGN KEY (`cod_prato`) REFERENCES `pratos` (`cod_prato`);
-
---
--- Restrições para tabelas `pedidos`
---
-ALTER TABLE `pedidos`
-  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`cod_cliente`) REFERENCES `clientes` (`cod_cliente`);
+ALTER TABLE `usuarios`
+  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
